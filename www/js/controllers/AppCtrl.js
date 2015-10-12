bce2b6f2d05c76664e50230153aea6562765f0f0
Marcel Hofmann <marcelhofmann@live.de>
2015-10-12 16:41:16 +0200

    Signup Form

--- www/js/controllers/AppCtrl.js
+++ www/js/controllers/AppCtrl.js
@@ -4,30 +4,30 @@ angular.module('bluevoo.controllers')
   $scope.loginData = {};
 
   // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
+  $ionicModal.fromTemplateUrl('templates/signup.html', {
     scope: $scope
   }).then(function(modal) {
     $scope.modal = modal;
   });
 
   // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
+  $scope.closeSignup = function() {
     $scope.modal.hide();
   };
 
   // Open the login modal
-  $scope.login = function() {
+  $scope.signup = function() {
     $scope.modal.show();
   };
 
   // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
+  $scope.doSignup = function() {
     console.log('Doing login', $scope.loginData);
 
     // Simulate a login delay. Remove this and replace with your login
     // code if using a login system
     $timeout(function() {
-      $scope.closeLogin();
+      $scope.closeSignup();
     }, 1000);
   };
 });

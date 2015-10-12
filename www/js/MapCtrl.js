aea5e89704be47edae87d5fd8c8908dfcb6d6526
Marcel Hofmann <marcelhofmann@live.de>
2015-10-12 15:07:56 +0200

    Clean Up and Google Maps

---
+++ www/js/MapCtrl.js
@@ -0,0 +1,14 @@
+angular.module('bluevoo')
+
+.controller('MapCtrl', function($scope, uiGmapGoogleMapApi) {
+  $scope.map = {
+    center: {
+      latitude: 48.6640875,
+      longitude: 9.0346769
+    },
+    zoom: 8
+  };
+  uiGmapGoogleMapApi.then(function(maps) {
+    console.log(maps);
+  });
+});
---
+++ www/js/routes.js
@@ -0,0 +1,21 @@
+angular.module('bluevoo').config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('app', {
+      url: '/app',
+      abstract: true,
+      templateUrl: 'templates/menu.html',
+      controller: 'AppCtrl'
+    })
+    .state('app.map', {
+      url: '/map',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/map.html',
+          controller: 'MapCtrl'
+        }
+      }
+    });
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/playlists');
+});

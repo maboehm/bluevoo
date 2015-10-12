0cdf504431a131816ecf37a5f9daed7559def03f
Marcel Hofmann <marcelhofmann@live.de>
2015-10-13 01:22:18 +0200

    checkin wireframe

--- www/js/controllers/CheckinCtrl.js
+++ www/js/controllers/CheckinCtrl.js
@@ -2,5 +2,19 @@ angular.module('bluevoo.controllers')
 
 .controller('CheckinCtrl', function($scope) {
   $scope.text = "Check in to location";
+  $scope.checkin = {};
 
+  $scope.places = [
+    "IBM Böblingen",
+    "IBM Herrenberg",
+    "IBM Ehningen",
+    "Other"
+  ];
+
+  $scope.types = ['IBM', 'Hotel', 'Kunde'];
+
+  $scope.placeChanged = function() {
+    console.log('hi');
+    console.log($scope.checkin);
+  };
 });
